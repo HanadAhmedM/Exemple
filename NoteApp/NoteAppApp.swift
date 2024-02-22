0226f04b3ede72c3f07cd6597ea226bfe3e8a530
hanad.ahmed <Hanadahmed1414@gmail.com>
2024-02-23 00:29:05 +0100

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Hanad.Ahmed on 2024-02-23.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
